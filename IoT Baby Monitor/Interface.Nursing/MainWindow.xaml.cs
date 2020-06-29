1660f598404d7b4a0730871efb197e26bbbc9c30
kaz1m1r <c.haan98@protonmail.com>
2020-06-29 13:41:36 +0200

    Casper's branch made

--- IoT Baby Monitor/Interface.Nursing/MainWindow.xaml.cs
+++ IoT Baby Monitor/Interface.Nursing/MainWindow.xaml.cs
@@ -10,19 +10,24 @@ using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
 using System.Windows.Shapes;
 
 namespace BabyphoneIoT.Interface.Nursing
 {
     /// <summary>
-    /// Interaction logic for MainWindow.xaml
+    /// Interaction logic for Window1.xaml
     /// </summary>
-    public partial class MainWindow : Window
+    public partial class Window1 : Window
     {
-        public MainWindow()
+        public Window1()
         {
             InitializeComponent();
+        }
+
+        private void Login_Button_Click(object sender, RoutedEventArgs e)
+        {
+            MainWindow m1 = new MainWindow();
+            m1.Show();
         }
     }
 }
